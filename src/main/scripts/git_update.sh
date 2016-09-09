4f2ad65a9e491f648b560b858173e068857752b2
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2016-09-09 01:12:56 -0300

    Deleting old files.

--- src/main/scripts/git_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-date + '%d/%m/%Y %H:%M:%S'
-echo "Updating Git repositories..."
-/usr/bin/find /local/Git -maxdepth 1 -type d -print -execdir git --git-dir={}/.git --work-tree=/local/Git/{} pull --all \;
-echo "Git repositories were updated."
