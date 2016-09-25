2e48cfec5213070d892be1a2f09fc0ceb4ce1dcc
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2016-09-25 17:08:39 -0300

    Showing cochanges and if it is probably to occur or not.

--- recominer-web/src/main/java/br/edu/utfpr/recominer/web/dto/CochangeDTO.java
+++ recominer-web/src/main/java/br/edu/utfpr/recominer/web/dto/CochangeDTO.java
@@ -10,10 +10,14 @@ public class CochangeDTO {
 
     private Integer id;
     private FileDTO file;
+    private String predictionResult;
+    private String algorithmName;
 
     public CochangeDTO(MachineLearningPrediction cochange) {
         this.id = cochange.getId();
         this.file = new FileDTO(cochange.getPredictedFile());
+        this.predictionResult = cochange.getPredictionResult();
+        this.algorithmName = cochange.getAlgorithmType();
     }
 
     public Integer getId() {
@@ -30,6 +34,22 @@ public class CochangeDTO {
 
     public void setFile(FileDTO file) {
         this.file = file;
+    }
+
+    public String getPredictionResult() {
+        return predictionResult;
+    }
+
+    public void setPredictionResult(String predictionResult) {
+        this.predictionResult = predictionResult;
+    }
+
+    public String getAlgorithmName() {
+        return algorithmName;
+    }
+
+    public void setAlgorithmName(String algorithmName) {
+        this.algorithmName = algorithmName;
     }
 
 }
