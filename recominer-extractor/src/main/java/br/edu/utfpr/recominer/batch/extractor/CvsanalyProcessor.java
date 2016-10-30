a091f38af898b07df387e3810a4bf8afb96d53ca
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2016-10-30 15:25:24 -0200

    Added option to skip CVSAanalY extensions.

--- recominer-extractor/src/main/java/br/edu/utfpr/recominer/batch/extractor/CvsanalyProcessor.java
+++ recominer-extractor/src/main/java/br/edu/utfpr/recominer/batch/extractor/CvsanalyProcessor.java
@@ -5,6 +5,7 @@ import br.edu.utfpr.recominer.externalprocess.ExternalProcess;
 import java.io.IOException;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -13,12 +14,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
  * @author Rodrigo T. Kuroda <rodrigokuroda at alunos.utfpr.edu.br>
  */
 @Named
+@StepScope
 public class CvsanalyProcessor {
 
     @Inject
     private JdbcTemplate template;
     
-    @Value("${runCsvanalyExtension:true}")
+    @Value("#{jobParameters[runCsvanalyExtension]}")
     private String runCsvanalyExtension;
 
     public int process(Project project) throws IOException, InterruptedException {
