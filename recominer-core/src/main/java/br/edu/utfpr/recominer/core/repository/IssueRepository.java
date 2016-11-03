c9eb1d21070d57c6f201f3574b2ce3ce21b4e212
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2016-11-03 00:50:17 -0200

    Fix query to get issues processed.

--- recominer-core/src/main/java/br/edu/utfpr/recominer/core/repository/IssueRepository.java
+++ recominer-core/src/main/java/br/edu/utfpr/recominer/core/repository/IssueRepository.java
@@ -201,7 +201,6 @@ public class IssueRepository extends JdbcRepository<Issue, Integer> {
                         + "  JOIN {0}_issues.issues_ext_jira iej ON iej.issue_id = i.id "
                         + "  JOIN {0}_vcs.scmlog s ON i2s.scmlog_id = s.id "
                         + "  JOIN {0}.files_commits fc ON i2s.scmlog_id = fc.commit_id "
-                        + "  JOIN {0}.files_commits fc ON i2s.scmlog_id = fc.commit_id "
                         + " WHERE s.id IN (SELECT commit_id FROM {0}.ml_predictions UNION SELECT commit_id FROM {0}.ar_predictions)",
                         project),
                 ROW_MAPPER);
