0b2f11c09282dcf5fe0044cd9ece11718a9e52eb
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2016-11-11 22:56:27 -0200

    Simplified configuration of database.

--- .travis.install-mysql-5.7.sh
+++
@@ -1,3 +0,0 @@
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again root'
-sudo apt-get -y install mysql-server
