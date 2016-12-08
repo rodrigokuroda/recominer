126fb04da1d3b8b6303b5009d65939547444b37a
Rodrigo Kuroda <rodrigokuroda@gmail.com>
2016-12-08 15:27:50 -0200

    Environment setup for experiment.

---
+++ recominer-web/docker/setupExperiment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: Rodrigo Kuroda <rodrigokuroda@alunos.utfpr.edu.br>
+set -e
+
+echo "Downloading databases..."
+if [ ! -f ~/recominer/experiment-db.zip ]; then 
+    wget -P ~/recominer https://github.com/rodrigokuroda/recominer/releases/download/0.7/experiment-db.zip
+    unzip -d ~/recominer ~/recominer/experiment-db.zip
+fi
+
+echo "Restoring databases..."
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS cxf_issues CHARACTER SET utf8 COLLATE utf8_general_ci"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS cxf_vcs CHARACTER SET utf8 COLLATE utf8_general_ci" 
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS cxf CHARACTER SET utf8 COLLATE utf8_general_ci" 
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS recominer" 
+mysql -uroot cxf_issues < ~/recominer/cxf_issues.sql
+mysql -uroot cxf_vcs < ~/recominer/cxf_vcs.sql
+mysql -uroot cxf < ~/recominer/cxf.sql
+mysql -uroot recominer < ~/recominer/recominer.sql
+
+echo "Starting Recominer..."
+if [ ! -f ~/recominer/recominer-web.zip ]; then 
+	wget -P ~/recominer https://github.com/rodrigokuroda/recominer/releases/download/0.7/recominer-web-0.7.jar
+fi
+
+java -Xmx1024m -Xms256m -Xss256k -jar ~/recominer/recominer-web-0.7.jar
